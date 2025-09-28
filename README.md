f128084d16551bf05320a2dfb5821eb4b61c829d
Domio <domiomc12@gmail.com>
2025-09-28 12:23:20 +0200

    readme init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # FlowScript
+
+## Compile from source
+
+- Install go (◕ᴥ◕ʋ)
+- Simply run `go build <path to main.go>`
+
+## Running code
+
+- Put FlowScript into you environment variables
+- Run `FlowScript <path to idk.fls>`
+
+## Contributing
+
+- Idk do whatever you want
+- Just don't destroy it
--- src/lexer/tokenize.go
+++ src/lexer/tokenize.go
@@ -52,6 +52,7 @@ func CreateLexer(source string) *Lexer {
 		pattern: []RegexPattern{
 			//numbers strings etc.
 			{regexp.MustCompile(`[0-9]+(\.[0-9]+)?`), numberHandler},
+			{regexp.MustCompile(`\s+`), skipHandler},
 			//default handlers
 			{regexp.MustCompile(`\[`), defaultHandler(OPEN_BRACKET, "[")},
 			{regexp.MustCompile(`\]`), defaultHandler(OPEN_BRACKET, "]")},
@@ -59,8 +60,15 @@ func CreateLexer(source string) *Lexer {
 			{regexp.MustCompile(`\}`), defaultHandler(CLOSE_CURLY, "}")},
 			{regexp.MustCompile(`\(`), defaultHandler(OPEN_PAREN, "(")},
 			{regexp.MustCompile(`\)`), defaultHandler(CLOSE_PAREN, ")")},
-			{regexp.MustCompile(`\==`), defaultHandler(EQUALS, "==")},
-			{regexp.MustCompile(`\!=`), defaultHandler(NOT_EQUALS, "!=")},
+			{regexp.MustCompile(`==`), defaultHandler(EQUALS, "==")},
+			{regexp.MustCompile(`!=`), defaultHandler(NOT_EQUALS, "!=")},
+			{regexp.MustCompile(`=`), defaultHandler(ASSIGNMENT, "=")},
+			{regexp.MustCompile(`!`), defaultHandler(NOT, "!")},
+			{regexp.MustCompile(`<=`), defaultHandler(LESS_EQUALS, "<=")},
+			{regexp.MustCompile(`<`), defaultHandler(LESS, "<")},
+			{regexp.MustCompile(`>=`), defaultHandler(GREATER_EQUALS, ">=")},
+			{regexp.MustCompile(`>`), defaultHandler(GREATER, ">")},
+			{regexp.MustCompile(`\|\|`), defaultHandler(OR, "||")},
 			{regexp.MustCompile(`\+=`), defaultHandler(PLUS_EQUALS, "+=")},
 			{regexp.MustCompile(`\+`), defaultHandler(PLUS, "+")},
 			{regexp.MustCompile(`-=`), defaultHandler(MINUS_EQUALS, "-=")},
@@ -105,4 +113,9 @@ func numberHandler(lex *Lexer, regex *regexp.Regexp) {
 	match := regex.FindString(lex.remainder())
 	lex.push(NewToken(NUMBER, match))
 	lex.advanceN(len(match))
+}
+
+func skipHandler(lex *Lexer, regex *regexp.Regexp) {
+	match := regex.FindStringIndex(lex.remainder())
+	lex.advanceN(match[1])
 }
