25f6ed0bc09ab0171629ef90bace97cb492bfb64
Domio <domiomc12@gmail.com>
2025-10-08 11:26:18 +0200

    initial commit

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NAME="Flow"
-
-if [[ "$OS" == "Windows_NT" ]]; then
-    NAME="Flow.exe"
-fi
-
-go build -o "$NAME" ./src
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/DomioKing653/FlowScript
-
-go 1.23.2
-
-require github.com/sanity-io/litter v1.5.8 // indirect
